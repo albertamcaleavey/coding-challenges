a29b5e635c1292caf75bcb5cf968ed0137c795fd
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-05-29 22:52:24 -0400

    add card deck challenge

--- app.js
+++ app.js
@@ -227,3 +227,13 @@ function between(a, b) {
 
 
 console.log(between(1, 4))
+
+//-------------------------------------------------------------------
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+// Our deck (is preloaded):
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
